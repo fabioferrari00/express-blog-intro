4d142822396990687c9974c9e0c95f6cf1ca444b
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-31 14:23:28 +0200

    [feat] add public folder and array posts

--- app.js
+++ app.js
@@ -2,6 +2,38 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const posts = [
+  {
+    title: "Napoli",
+    content: "lorem ipsum dolor",
+    img: "/imgs/city/Napoli.jpeg",
+    tags: ['Mario', 'Filippo', 'Maria', 'Fabio']
+  },
+  {
+    title: "Roma",
+    content: "lorem ipsum dolor",
+    img: "/imgs/city/Roma.jpeg",
+    tags: ['Mario', 'Andrea', 'Maria', 'Luca']
+  },
+  {
+    title: "Milano",
+    content: "lorem ipsum dolor",
+    img: "/imgs/city/Milano.jpeg",
+    tags: ['Mario', 'Filippo', 'Maria', 'Fabio', 'Gianluca']
+  },
+  {
+    title: "Torino",
+    content: "lorem ipsum dolor",
+    img: "/imgs/city/Torino.jpeg",
+    tags: ['Mario', 'Filippo', 'Andrea']
+  },
+  {
+    title: "Bergamo",
+    content: "lorem ipsum dolor",
+    img: "/imgs/city/Bergamo.jpeg",
+    tags: ['Maria', 'Fabio', 'Giusy']
+  },
+]
 
 app.get('/', (req, res) => {
   console.log('Server del mio blog');
