3fc17b8655cab7875bd4a161600213a471b24dc0
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-31 14:30:07 +0200

    [fix] array posts in /bacheca

--- app.js
+++ app.js
@@ -5,44 +5,44 @@ const port = 3000;
 app.use(express.static('public'));
 
 
-const posts = [
-  {
-    title: "Napoli",
-    content: "lorem ipsum dolor",
-    img: "/imgs/city/Napoli.jpeg",
-    tags: ['Mario', 'Filippo', 'Maria', 'Fabio']
-  },
-  {
-    title: "Roma",
-    content: "lorem ipsum dolor",
-    img: "/imgs/city/Roma.jpeg",
-    tags: ['Mario', 'Andrea', 'Maria', 'Luca']
-  },
-  {
-    title: "Milano",
-    content: "lorem ipsum dolor",
-    img: "/imgs/city/Milano.jpeg",
-    tags: ['Mario', 'Filippo', 'Maria', 'Fabio', 'Gianluca']
-  },
-  {
-    title: "Torino",
-    content: "lorem ipsum dolor",
-    img: "/imgs/city/Torino.jpeg",
-    tags: ['Mario', 'Filippo', 'Andrea']
-  },
-  {
-    title: "Bergamo",
-    content: "lorem ipsum dolor",
-    img: "/imgs/city/Bergamo.jpeg",
-    tags: ['Maria', 'Fabio', 'Giusy']
-  },
-]
 
 app.get('/', (req, res) => {
   console.log('Server del mio blog');
 })
 
 app.get('/bacheca', (req, res) => {
+  const posts = [
+    {
+      title: "Napoli",
+      content: "lorem ipsum dolor",
+      img: "/imgs/city/Napoli.jpeg",
+      tags: ['Mario', 'Filippo', 'Maria', 'Fabio']
+    },
+    {
+      title: "Roma",
+      content: "lorem ipsum dolor",
+      img: "/imgs/city/Roma.jpeg",
+      tags: ['Mario', 'Andrea', 'Maria', 'Luca']
+    },
+    {
+      title: "Milano",
+      content: "lorem ipsum dolor",
+      img: "/imgs/city/Milano.jpeg",
+      tags: ['Mario', 'Filippo', 'Maria', 'Fabio', 'Gianluca']
+    },
+    {
+      title: "Torino",
+      content: "lorem ipsum dolor",
+      img: "/imgs/city/Torino.jpeg",
+      tags: ['Mario', 'Filippo', 'Andrea']
+    },
+    {
+      title: "Bergamo",
+      content: "lorem ipsum dolor",
+      img: "/imgs/city/Bergamo.jpeg",
+      tags: ['Maria', 'Fabio', 'Giusy']
+    },
+  ]
   res.json(posts);
 })
 
